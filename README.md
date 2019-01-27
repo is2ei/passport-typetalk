5d7154338dc81ef74ee7f6825cc5018d60e46c45
Issei Horie <issei.horie@nulab-inc.com>
2019-01-28 00:07:09 +0900

    Set lint

--- README.md
+++ README.md
@@ -17,4 +17,39 @@ $ npm install --save-dev passport-typetalk
 
 ## Usage
 
+Express example
 
+```
+const {TYPETALK_CLIENT_ID, TYPETALK_CLIENT_SECRET} = process.env;
+const Express = require('express');
+const TypetalkStrategy = require('passport-typetalk').Strategy
+const passport = require('passport');
+
+const app = Express();
+
+passport.use(new TypetalkStrategy({
+    clientID: TYPETALK_CLIENT_ID,
+    clientSecret: TYPETALK_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/typetalk/callback",
+    scope: ['my', 'topic.read']
+  }, (accessToken, refreshToken, profile, done) => {
+    // optionally persist profile data
+    done(null, profile);
+  }
+));
+
+app.use(passport.initialize());
+
+app.get('/', function(req, res) {
+  res.send('<a href="/auth/typetalk">Login with Typetalk</a>')
+})
+
+app.get('/auth/typetalk', passport.authorize('typetalk'));
+
+app.get('/auth/typetalk/callback',
+  passport.authorize('typetalk', { failureRedirect: '/' }),
+  (req, res) => res.redirect('/')
+);
+
+app.listen(3000);
+```
--- test/strategy.test.js
+++ test/strategy.test.js
@@ -1,14 +1,14 @@
 var chai = require('chai')
   , TypetalkStrategy = require('../lib/strategy');
 
+
 describe('Strategy', function() {
    
   describe('constructed', function() {
     var strategy = new TypetalkStrategy({
       clientID: 'ABC123',
       clientSecret: 'secret'
-    },
-    function() {});
+    }, function() {});
 
     it('should be named typetalk', function() {
       expect(strategy.name).to.equal('typetalk');
@@ -23,11 +23,14 @@ describe('Strategy', function() {
     });
   })
 
-  describe('authorization request with display parameter', function() {
+  describe('authorization request with callbackURL and scope parameters', function() {
     var strategy = new TypetalkStrategy({
       clientID: 'ABC123',
-      clientSecret: 'secret'
+      clientSecret: 'secret',
+      callbackURL: "http://localhost:3000/auth/typetalk/callback",
+      scope: ['my', 'topic.read']
     }, function() {});
+
 
     var url;
 
@@ -39,12 +42,12 @@ describe('Strategy', function() {
         })
         .req(function(req) {
         })
-        .authenticate({ authType: 'reauthenticate', authNonce: 'foo123' });
+        .authenticate();
     });
 
     it('should be redirected', function() {
-      expect(url).to.equal('https://typetalk.com/oauth2/authorize?response_type=code&client_id=ABC123')
+      expect(url).to.equal('https://typetalk.com/oauth2/authorize?response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Ftypetalk%2Fcallback&scope=my%20topic.read&client_id=ABC123')
     });
-  });
+  })
 
 });
