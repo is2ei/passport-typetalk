d6a3785726525538c0cf53db96ea587fb72d2ec8
Issei Horie <issei.horie@nulab-inc.com>
2019-02-02 22:39:35 +0900

    Improve coverage

--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -1,3 +1,34 @@
-
 var TypetalkStrategy = require('../lib/strategy');
 
+
+describe('Strategy#userProfile', function() {
+
+  describe('fetched from default endpoint', function() {
+    var strategy = new TypetalkStrategy({
+      clientID: 'ABC123',
+      clientSecret: 'secret'
+    }, function() {});
+
+    strategy._oauth2.get = function(url, accessToken, callback) {
+      if (url != 'https://typetalk.com/api/v1/profile') { return callback(new Error('incorrect url argument')); }
+      if (accessToken != 'token') { return callback(new Error('incorrect token argument')); }
+
+      var body = '{"account":{}}';
+      callback(null, body, undefined);
+    };
+
+    var profile;
+    before(function(done) {
+      strategy.userProfile('token', function(err, p) {
+        if (err) { return done(err); }
+        profile = p;
+        done();
+      });
+    });
+
+    it('should parse profile', function() {
+      expect(profile.provider).to.equal('typetalk');
+    });
+
+  });
+});
