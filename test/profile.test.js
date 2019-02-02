ca289cafa37d046469d8796486efc8bd13e36609
Issei Horie <issei.horie@nulab-inc.com>
2019-02-03 01:53:19 +0900

    Improve coverage

--- test/profile.test.js
+++ test/profile.test.js
@@ -1,1 +1,16 @@
 var Profile = require('../lib/profile');
+
+describe('Profile#parse', function() {
+
+  describe('try to parse string', function() {
+    var profile;
+    before(function(done) {
+      profile = Profile.parse('{"account":{"id":12345}}');
+      done();
+    });
+
+    it('should parse profile', function() {
+      expect(profile.id).to.equal(12345);
+    });
+  });
+});
