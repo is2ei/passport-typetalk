9f0ad9166abdeed486e09e4210b69fb2462333af
Issei Horie <is2ei.horie@gmail.com>
2019-02-19 01:48:39 +0900

    Refactor

--- test/package.test.js
+++ test/package.test.js
@@ -3,13 +3,17 @@ var strategy = require("..");
 
 describe("passport-typetalk", function () {
 
+    it("should export Strategy constructor as module", function () {
+        expect(strategy).to.be.a("function");
+        expect(strategy).to.equal(strategy.Strategy);
+    });
+
     it("should export Strategy constructor", function () {
         expect(strategy.Strategy).to.be.a("function");
     });
 
-    it("should export Strategy constructor as module", function () {
-        expect(strategy).to.be.a("function");
-        expect(strategy).to.equal(strategy.Strategy);
+    it("should export Error constructors", function () {
+        expect(strategy.TypetalkAPIError).to.be.a("function");
     });
 
 });
--- test/strategy.test.js
+++ test/strategy.test.js
@@ -1,80 +1,158 @@
-/* eslint-disable func-names, max-lines-per-function */
+/*
+    eslint-disable
+    func-names,
+    max-lines-per-function,
+    max-len
+*/
 var chai = require("chai");
 var TypetalkStrategy = require("../lib/strategy");
 
 
-describe("Strategy", function () {
-    var strategy = {},
+describe("TypetalkStrategy", function () {
+
+    var ERR_MSG = "",
         url = "";
 
     describe("constructed", function () {
-        strategy = new TypetalkStrategy({
-            "clientID": "ABC123",
-            "clientSecret": "secret"
-        }, function () {
-            // Do nothing.
+
+        describe("with normal options", function () {
+            var strategyWithNormalOptions = new TypetalkStrategy({
+                "clientID": "ABC123",
+                "clientSecret": "secret"
+            }, function () {
+                // Do nothing.
+            });
+
+            it("should be named typetalk", function () {
+                expect(strategyWithNormalOptions.name).to.equal("typetalk");
+            });
         });
 
-        it("should be named typetalk", function () {
-            expect(strategy.name).to.equal("typetalk");
-        });
-    });
-
-    describe("constructed without options", function () {
-        it("should throw", function () {
-
-            expect(function () {
-                strategy = new TypetalkStrategy(null, function () {
-                    // Do nothing.
-                });
-                strategy();
-            }).to.throw(Error);
-        });
-    });
-
-    describe("constructed with undefined options", function () {
-        it("should throw", function () {
-            expect(function () {
-                strategy = new TypetalkStrategy({}, function () {
-                    // Do nothing.
-                });
-                strategy();
-            }).to.throw(Error);
-        });
-    });
-
-    describe("auth request with callbackURL and scope parameters", function () {
-        strategy = new TypetalkStrategy({
-            "callbackURL": "http://localhost:3000/auth/typetalk/callback",
-            "clientID": "ABC123",
-            "clientSecret": "secret",
-            "scope": [
-                "my",
-                "topic.read"
-            ]
-        }, function () {
-            // Do nothing.
-        });
-
-        before(function (done) {
-            chai.passport.use(strategy)
-                .redirect(function (u) {
-                    url = u;
-                    done();
+        describe("without a clientID option", function () {
+            ERR_MSG = "OAuth2Strategy requires a clientID option";
+            it("should throw", function () {
+                expect(function () {
+                    var strategy = new TypetalkStrategy({
+                        "clientSecret": "secret"
+                    }, function () {
+                        // Do nothing.
+                    });
+                    chai.passport.use(strategy)
+                        .redirect(function (u) {
+                            url = u;
+                        })
+                        .req(function () {
+                            // Do nothing.
+                        })
+                        .authenticate();
                 })
-                .req(function () {
-                    // Do nothing.
-                })
-                .authenticate();
+                    .to
+                    .throw(TypeError, ERR_MSG);
+            });
         });
 
-        it("should be redirected", function () {
-            expect(url).to.equal("https://typetalk.com/oauth2/authorize?" +
-            "response_type=code&" +
-            "redirect_uri=http%3A%2F%2Flocalhost" +
-            "%3A3000%2Fauth%2Ftypetalk%2Fcallback&" +
-            "scope=my%20topic.read&client_id=ABC123");
+        describe("without a clientSecret option", function () {
+            it("should not throw", function () {
+                expect(function () {
+                    var strategy = new TypetalkStrategy({
+                        "clientID": "ABC123"
+                    }, function () {
+                        // Do nothing.
+                    });
+                    chai.passport.use(strategy)
+                        .redirect(function (u) {
+                            url = u;
+                        })
+                        .req(function () {
+                            // Do nothing.
+                        })
+                        .authenticate();
+                }).to.not.throw();
+            });
         });
+
+    });
+
+    describe("issuing authorization request", function () {
+
+        describe("that redirects to service provider without redirect URI", function () {
+            var strategyWithoutCallback = new TypetalkStrategy({
+                "clientID": "ABC123",
+                "clientSecret": "secret"
+            }, function () {
+                // Do nothing.
+            });
+
+            before(function (done) {
+                chai.passport.use(strategyWithoutCallback)
+                    .redirect(function (u) {
+                        url = u;
+                        done();
+                    })
+                    .req(function () {
+                        // Do nothing.
+                    })
+                    .authenticate();
+            });
+
+            it("should be redirected", function () {
+                expect(url).to.equal("https://typetalk.com/oauth2/authorize?response_type=code&scope=my&client_id=ABC123");
+            });
+        });
+
+        describe("that redirects to service provider with redirect URI", function () {
+            var strategyWithCallBack = new TypetalkStrategy({
+                "callbackURL": "http://example.com/auth/typetalk/callback",
+                "clientID": "ABC123",
+                "clientSecret": "secret"
+            }, function () {
+                // Do nothing.
+            });
+
+            before(function (done) {
+                chai.passport.use(strategyWithCallBack)
+                    .redirect(function (u) {
+                        url = u;
+                        done();
+                    })
+                    .req(function () {
+                        // Do nothiung.
+                    })
+                    .authenticate();
+            });
+
+            it("should be redirected", function () {
+                expect(url).to.equal("https://typetalk.com/oauth2/authorize?response_type=code&redirect_uri=http%3A%2F%2Fexample.com%2Fauth%2Ftypetalk%2Fcallback&scope=my&client_id=ABC123");
+            });
+        });
+
+        describe("that redirects to service provider with redirect URI and scope", function () {
+            var strategyWithScope = new TypetalkStrategy({
+                "callbackURL": "http://example.com/auth/typetalk/callback",
+                "clientID": "ABC123",
+                "clientSecret": "secret",
+                "scope": "my topic.read"
+            }, function () {
+                // Do nothing.
+            });
+
+            before(function (done) {
+                chai.passport.use(strategyWithScope)
+                    .redirect(function (u) {
+                        url = u;
+                        done();
+                    })
+                    .req(function () {
+                        // Do nothiung.
+                    })
+                    .authenticate();
+            });
+
+            it("should be redirected", function () {
+                expect(url).to.equal("https://typetalk.com/oauth2/authorize?response_type=code&redirect_uri=http%3A%2F%2Fexample.com%2Fauth%2Ftypetalk%2Fcallback&scope=my%20topic.read&client_id=ABC123");
+            });
+        });
+
     });
 
 });
