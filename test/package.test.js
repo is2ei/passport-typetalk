dbc165f2d03f92ef01a22160f9cb773662638634
Issei Horie <is2ei.horie@gmail.com>
2019-03-09 11:50:11 +0900

    Enable func-names in package.test.js

--- test/package.test.js
+++ test/package.test.js
@@ -1,18 +1,17 @@
-/* eslint-disable func-names */
 const strategy = require("..");
 
-describe("passport-typetalk", function () {
+describe("passport-typetalk", () => {
 
-    it("should export Strategy constructor as module", function () {
+    it("should export Strategy constructor as module", () => {
         expect(strategy).to.be.a("function");
         expect(strategy).to.equal(strategy.Strategy);
     });
 
-    it("should export Strategy constructor", function () {
+    it("should export Strategy constructor", () => {
         expect(strategy.Strategy).to.be.a("function");
     });
 
-    it("should export Error constructors", function () {
+    it("should export Error constructors", () => {
         expect(strategy.TypetalkAPIError).to.be.a("function");
     });
 
