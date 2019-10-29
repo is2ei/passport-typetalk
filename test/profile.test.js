54869e2ef3f50000066f3d6e4b7a2bb8086ff0c0
Issei Horie <is2ei.horie@gmail.com>
2019-10-30 01:06:58 +0900

    Use plugin:mocha/recommended

--- test/profile.test.js
+++ test/profile.test.js
@@ -1,15 +1,17 @@
+/* eslint-disable func-names */
+
 const Profile = require("../lib/profile");
 
-describe("Profile#parse", () => {
+describe("Profile#parse", function () {
 
-    describe("try to parse string", () => {
+    describe("try to parse string", function () {
         let profile = {};
-        before((done) => {
+        before(function (done) {
             profile = Profile.parse('{"account":{"id":12345}}');
             done();
         });
 
-        it("should parse profile", () => {
+        it("should parse profile", function () {
             const profileId = 12345;
             expect(profile.id).to.equal(profileId);
         });
