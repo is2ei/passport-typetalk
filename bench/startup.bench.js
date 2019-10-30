620322f842e3558da88784428712b24b681f4b3a
Issei Horie <is2ei.horie@gmail.com>
2019-10-30 09:45:27 +0900

    Added benchmark

--- bench/startup.bench.js
+++ bench/startup.bench.js
@@ -1,20 +1,19 @@
-'use strict';
-
-const os = require('os');
-const { expect } = require('chai');
-const { performance } = require('perf_hooks');
+/* eslint-disable */
+const os = require("os");
+const { expect } = require("chai");
+const { performance } = require("perf_hooks");
 
 const [ { speed: cpuSpeed } ] = os.cpus();
 
 describe('startup / require time', function () {
-  it('should not take longer as the defined budget to require the plugin', function () {
-    const budget = 85000 / cpuSpeed;
+    it('should not take longer as the defined budget to require the plugin', function () {
+        const budget = 85000 / cpuSpeed;
 
-    const startTime = performance.now();
-    require('../lib/index');
-    const endTime = performance.now();
-    const loadTime = endTime - startTime;
+        const startTime = performance.now();
+        require('../lib/index');
+        const endTime = performance.now();
+        const loadTime = endTime - startTime;
 
-    expect(loadTime).to.be.below(budget);
-  })
-})
+        expect(loadTime).to.be.below(budget);
+    });
+});
