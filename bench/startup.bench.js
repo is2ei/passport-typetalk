4a780e0807d6da92cfefa35f47f642e50c0892bd
Issei Horie <is2ei.horie@gmail.com>
2019-10-31 09:20:19 +0900

    Improved bench

--- bench/startup.bench.js
+++ bench/startup.bench.js
@@ -2,18 +2,26 @@
 const os = require("os");
 const { expect } = require("chai");
 const { performance } = require("perf_hooks");
+const addContext = require('mochawesome/addContext');
 
 const [ { speed: cpuSpeed } ] = os.cpus();
 
 describe('startup / require time', function () {
-    it('should not take longer as the defined budget to require the plugin', function () {
+    it('measuring time', function () {
+
         const budget = 85000 / cpuSpeed;
 
-        const startTime = performance.now();
-        require('../lib/index');
-        const endTime = performance.now();
-        const loadTime = endTime - startTime;
+        let avgLoadTime = 0;
+        for (var i = 0; i < 100; i++) {
+            const startTime = performance.now();
+            require('../lib/index');
+            const endTime = performance.now();
+            avgLoadTime += endTime - startTime;
+        }
 
-        expect(loadTime).to.be.below(budget);
+        addContext(this, `Load Time: ${avgLoadTime / budget}`);
+        console.info(`startup / require time: ${avgLoadTime / budget}`);
+ 
+        expect(avgLoadTime).to.be.below(budget);
     });
 });
